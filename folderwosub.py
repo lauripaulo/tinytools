fa67ab524ce9c4ef49b1d13faa0d77dbbb7a431c
unknown <ateliware@SAXON.saxon>
2020-09-28 18:16:52 -0300

    Initial working version

--- folderwosub.py
+++ folderwosub.py
@@ -33,14 +33,47 @@
 
 import os
 import json
+import argparse
+import pathlib
 
 from pathlib import Path
 from progress.bar import Bar
 from progress.spinner import Spinner
 
+def findAllFolders(folder):
+    folders = [f.path for f in os.scandir(folder) if f.is_dir()]
+    print("\nFound {} folders.".format(len(folders)))
+    return folders
+
+def main(folder):
+    foundFolderList = []
+    folders = findAllFolders(folder)
+    for folder in folders:
+        subFolders = [f.path for f in os.scandir(folder) if f.is_dir()]
+        if len(subFolders) == 0:
+            foundFolderList.append(folder)
+            print("Found folder: '{}'".format(folder))
+    return foundFolderList
+
 def find_folders_without_subfolders():
+    folder = args.folder
+    logFile = args.logfile
+    print("\n{}".format(parser.description))
+    print("Folder: '{}'".format(folder))
+    print("Log file: '{}'".format(logFile))
+    foundFolderList = main(folder)
+    with open(os.path.join(pathlib.Path().absolute(), logFile), "w+") as log_file:
+        json.dump(foundFolderList, log_file)
     pass
 
 if __name__=="__main__":
-    find_folders_without_subfolders()
-    SystemExit()
+    parser = argparse.ArgumentParser(description="Folder Without SubFolders - v0.1")
+    parser.add_argument("folder", metavar="folder", type=str, 
+        help="folder to search.")
+    parser.add_argument("logfile", metavar="logfile", type=str, 
+        help="log file where the results are kept.")
+    args = parser.parse_args()
+    if not args.folder or not args.logfile:
+        parser.print_help()
+    else:
+        find_folders_without_subfolders()
