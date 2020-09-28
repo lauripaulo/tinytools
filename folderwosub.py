4449c17123a5f9ba3197cff90abcc424cd636cec
unknown <ateliware@SAXON.saxon>
2020-09-28 17:45:53 -0300

    List forlders withou subfolders

---
+++ folderwosub.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+# MIT License
+
+# Copyright (c) 2020 Lauri P. Laux Jr
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+#
+# Find images with less than the designed size and move them to a 
+# folder for inspection. The aim is to move all screenshots and
+# junk images from social media from a folder of your own
+# phone/camera "good" photos.
+#
+# Lauri P. Laux - lauripaulo@hotmail.com
+# 2020 - Corona virus code boredom challange :-)
+
+import os
+import json
+
+from pathlib import Path
+from progress.bar import Bar
+from progress.spinner import Spinner
+
+def find_folders_without_subfolders():
+    pass
+
+if __name__=="__main__":
+    find_folders_without_subfolders()
+    SystemExit()
